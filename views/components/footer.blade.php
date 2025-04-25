b352c1292e84d553daa30601ea60492b8198c716
lekok indah lia <144763130+lillia05@users.noreply.github.com>
2025-04-25 10:54:54 +0700

    Update footer.blade.php

--- views/components/footer.blade.php
+++ views/components/footer.blade.php
@@ -1,17 +1,17 @@
 <footer class="bg-green-900 text-white px-6 py-6 mt-8">
     <div class="flex flex-col md:flex-row justify-between text-sm">
         <div class="mb-4 md:mb-0">
-            <img src="\img\logo-simapan.png" class="w-32 mb-2" alt="SIMAPAN Logo">
+            <img src="icon/logo_simapan_lengkap.png" class="w-32 mb-2" alt="SIMAPAN Logo">
             <p>Sistem Informasi Manajemen Pertanian Kabupaten Lampung Barat</p>
-            <p class="mt-2 text-xs">© Copyright 2025. Kelompok Lincari - Jurusan Ilmu Komputer, Universitas Lampung
+            <p class="mt-2 text-xs">© Copyright 2025. Kelompok Linasari - Jurusan Ilmu Komputer, Universitas Lampung
             </p>
         </div>
         <div>
             <p>Kantor Dinas Pertanian Kab. Lambar</p>
             <p>Jl. Raden Intan, Way Mengaku, Komp. Perkantoran Pemkab Lampung Barat, Liwa, Lampung, 34842</p>
-            <p>📞 Telp: 1234-5678-910</p>
-            <p>📱 WhatsApp: 0812-3456-7890</p>
-            <p>📧 Email: dinaspertanianlambar@gmail.com</p>
+            <img src="icon/telepon_footer.png" class="w-5 mb-1" alt="telepon Logo"> <p> Telp: 1234-5678-910</p>
+            <img src="icon/whatsapp_footer.png" class="w-5 mb-1" alt="whatsapp Logo"> <p> WhatsApp: 0812-3456-7890</p>
+            <img src="icon/email_footer.png" class="w-5 mb-1" alt="email Logo"> <p> Email: dinaspertanianlambar@gmail.com</p>
         </div>
     </div>
 </footer>
